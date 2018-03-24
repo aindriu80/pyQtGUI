c0c67bea9e39faf10935cfe67bbcef097d100331
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-03-24 10:07:32 +0000

    Declarative functions

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 #!/user/bin/python3
 
+import os, sys
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-
-class Page(QWidget):
-    def __init__(self, parent=None):
-        super(Page, self).__init__(parent)
-
-        my_label = QLabel("This is my label")
-        layout = QVBoxLayout()
-
-        layout.addWidget(my_label)
-
-        mainLayout = QGridLayout()
-        mainLayout.addLayout(layout, 0, 1)
-
-        self.setLayout(mainLayout)
-        self.setWindowTitle("My first Qt app")
+from PyQt5.QtGui import *
+from PyQt5.QtQuick import *
+from PyQt5.Qt import *
 
 if __name__ == "__main__":
-    import sys
-
     app = QApplication(sys.argv)
 
-    window = Page()
-    window.show()
+    engine = QQmlApplicationEngine()
+    engine.load(QUrl.fromLocalFile("main.qml"))
 
     sys.exit(app.exec_())
