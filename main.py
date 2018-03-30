f701928dfd59cccfd9fc076db3b0ab93297843bd
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-03-30 17:11:41 +0100

    Evaluating queries

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import math
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -7,11 +8,22 @@ class Button:
     def __init__(self, text, results):
         self.b = QPushButton(str(text))
         self.text = text
-        self.result = results
+        self.results = results
         self.b.clicked.connect(lambda: self.handleInput(self.text))
 
     def handleInput(self, v):
-        print("clicked", v)
+        if v == "=":
+            res = eval(self.results.text())
+            self.results.setText(str(res))
+        elif v == "AC":
+            self.results.setText("")
+        elif v == "√":
+            value = float(self.results.text())
+            self.results.setText(str(math.sqrt(value)))
+        else:
+            current_value = self.results.text()
+            new_value = current_value + str(v)
+            self.results.setText(new_value)
 
 class Application(QWidget):
     def __init__(self):
@@ -23,16 +35,16 @@ class Application(QWidget):
         # Create our grid
         grid = QGridLayout()
         results = QLineEdit()
-        buttons = ["AC", "C", "CE", "/",
+        buttons = ["AC", "√", "DEL", "/",
                    7, 8, 9, "*",
                    4, 5, 6, "-",
                    1, 2, 3, "+",
                    0, ".", "="]
 
+        grid.addWidget(results, 0, 0, 1, 4)
+
         row = 1
         col = 0
-
-        grid.addWidget(results, 0, 0, 1, 4)
 
         for button in buttons:
             if col > 3:
@@ -45,7 +57,7 @@ class Application(QWidget):
                 grid.addWidget(buttonObject.b, row, col, 1, 2)
                 col += 1
             else:
-                grid.addWidget(buttonObject.b, row, col,1,1)
+                grid.addWidget(buttonObject.b, row, col, 1, 1)
 
             col += 1
 
